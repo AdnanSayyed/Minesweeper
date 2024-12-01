1a2d9d943b6cb4cff01420827f244c08b6c1848b
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-12-01 15:21:22 +0530

    Slice Cell Texture

--- Array-Minesweeper/header/Gameplay/Cell/CellController.h
+++ Array-Minesweeper/header/Gameplay/Cell/CellController.h
@@ -6,21 +6,31 @@ namespace Gameplay
     namespace Cell
     {
         class CellView;
+        class CellModel;
+
+        enum class CellState;
+        enum class CellValue;
 
         class CellController
         {
         private:
             CellView* cell_view;
+            CellModel* cell_model;
 
             void destroy();
 
         public:
-            CellController();
+            CellController(sf::Vector2i grid_position);
             ~CellController();
 
             void initialize(float cell_width, float cell_height);
             void update();
             void render();
+
+            void reset();
+            CellState getCellState();
+            CellValue getCellValue();
+            sf::Vector2i getCellPosition();
         };
     }
 }
